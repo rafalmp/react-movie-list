f29a68fc9ca2af6999f199e6b7850ad70a8e244a
Rafał Peisert <rafalmp@users.noreply.github.com>
2021-01-30 17:25:16 +0100

    Retrieve data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,12 @@
-const Card = () => {
-  return <h2>Movie #1</h2>
+const Card = ({ movie }) => {
+  return (
+    <div>
+      <h2>{`#${movie.ranking} - ${movie.title} (${movie.year})`}</h2>
+      <img src={movie.img.src} alt={movie.img.alt} width={'200'} />
+      <p>{`Distributor: ${movie.distributor}`}</p>
+      <p>{`Amount: ${movie.amount}`}</p>
+    </div>
+  )
 }
 
 export default Card
--- src/containers/List.js
+++ src/containers/List.js
@@ -2,7 +2,31 @@ import { Component } from 'react';
 import Card from "../components/Card";
 
 export default class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loading: true,
+    }
+  }
+
+  async componentDidMount() {
+    const movies = await fetch('assets/data.json');
+    const moviesJSON = await movies.json()
+    if (moviesJSON) {
+      this.setState({
+        data: moviesJSON,
+        loading: false,
+      });
+    }
+  }
+
   render() {
-    return <Card />;
+    const { data, loading } = this.state;
+    if (loading) {
+      return <div>Loading...</div>
+    }
+
+    return data.map(movie => <Card key={ movie.id } movie={ movie } />);
   }
 }
