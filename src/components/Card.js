2ed53adce9bde8165763aab08fb6ea26a408070c
Rafał Peisert <rafalmp@users.noreply.github.com>
2021-01-30 17:00:12 +0100

    Create components

---
+++ src/components/Card.js
@@ -0,0 +1,5 @@
+const Card = () => {
+  return <h2>Movie #1</h2>
+}
+
+export default Card
---
+++ src/containers/List.js
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+import Card from "../components/Card";
+
+export default class List extends Component {
+  render() {
+    return <Card />;
+  }
+}
